f4f9ef95fa65260ac5cdc7858a1caf62a0ed9ba6
Halimat Mercy Oseni <mercy.oseni@andela.com>
2019-07-27 16:22:58 +0100

    Add filter for cap-shape attribute

--- app/services/filter_mushroom.rb
+++ app/services/filter_mushroom.rb
@@ -1,9 +1,11 @@
 class FilterMushroom
   def initialize(filter_params)
     @mushroom_class = filter_params[:mushroom_class]
+    @cap_shape = filter_params[:cap_shape]
   end
 
   def run
-    Mushroom.where(mushroom_class: @mushroom_class).limit(10)
+    Mushroom.where(mushroom_class: @mushroom_class).limit(10) if @mushroom_class.present?
+    Mushroom.where(cap_shape: @cap_shape).limit(10) if @cap_shape.present?
   end
 end
