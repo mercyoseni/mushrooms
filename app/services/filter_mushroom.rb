23cc19c06df7a8f3d3ebd41b7766708749b82a00
Halimat Mercy Oseni <mercy.oseni@andela.com>
2019-07-27 18:29:44 +0100

    Add filter for cap_surface, cap_color, bruises and odor attributes

--- app/services/filter_mushroom.rb
+++ app/services/filter_mushroom.rb
@@ -6,6 +6,10 @@ class FilterMushroom
     @params = params
     @mushroom_class = params[:filter][:mushroom_class]
     @cap_shape = params[:filter][:cap_shape]
+    @cap_surface = params[:filter][:cap_surface]
+    @cap_color = params[:filter][:cap_color]
+    @bruises = params[:filter][:bruises]
+    @odor = params[:filter][:odor]
   end
 
   def run
@@ -13,6 +17,10 @@ class FilterMushroom
 
     mushroom_ids << Mushroom.where(mushroom_class: @mushroom_class).pluck(:id) if @mushroom_class.present?
     mushroom_ids << Mushroom.where(cap_shape: @cap_shape).pluck(:id) if @cap_shape.present?
+    mushroom_ids << Mushroom.where(cap_surface: @cap_surface).pluck(:id) if @cap_surface.present?
+    mushroom_ids << Mushroom.where(cap_color: @cap_color).pluck(:id) if @cap_color.present?
+    mushroom_ids << Mushroom.where(bruises: @bruises).pluck(:id) if @bruises.present?
+    mushroom_ids << Mushroom.where(odor: @odor).pluck(:id) if @odor.present?
 
     Mushroom.where(id: mushroom_ids.flatten).page(params[:page])
   end
