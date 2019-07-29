02ab4817ffc786715c210caf42dbb59e45cdc327
Halimat Mercy Oseni <mercy.oseni@andela.com>
2019-07-29 01:04:39 +0100

    Assign page params to a variable

--- app/controllers/mushrooms_controller.rb
+++ app/controllers/mushrooms_controller.rb
@@ -3,13 +3,14 @@ class MushroomsController < ApplicationController
 
   def index
     @mushrooms = []
+    page = params[:page]
 
     if search_query.present?
-      @mushrooms = SearchMushroom.new(search_query, params[:page]).run
+      @mushrooms = SearchMushroom.new(search_query, page).run
     end
 
     if params[:filter].present?
-      @mushrooms = FilterMushroom.new(params[:filter], params[:page]).run
+      @mushrooms = FilterMushroom.new(params[:filter], page).run
     end
   end
 end
